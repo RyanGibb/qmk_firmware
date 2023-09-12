676a0263d09a96b3a02a76ca2974dc4edc3282b4
Ryan Gibb <ryan@freumh.org>
2023-09-12 23:04:30 +0100

    sway escape and grave

--- keyboards/moonlander/keymaps/ryan/keymap.c
+++ keyboards/moonlander/keymaps/ryan/keymap.c
@@ -17,8 +17,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // base
   [0] = LAYOUT_moonlander(
     XXXXXXX,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           XXXXXXX,                       XXXXXXX,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           XXXXXXX,
-    KC_ESC,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRC,                       KC_RBRC,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_DEL,
-    UK_GRV,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_MINUS,                      KC_EQUAL,       KC_H,           KC_J,           KC_K,           KC_L,           KC_SEMICOLON,   UK_QUOT,
+    UK_GRV,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRC,                       KC_RBRC,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_DEL,
+    KC_ESC,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_MINUS,                      KC_EQUAL,       KC_H,           KC_J,           KC_K,           KC_L,           KC_SEMICOLON,   UK_QUOT,
     UK_BSLS,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLSH,        UK_HASH,
     OSL(1),         KC_LGUI,        KC_LALT,        KC_LCTL,        KC_LSFT,                        TG(2),                         TG(3),                          KC_RSFT,        KC_RCTL,        KC_RALT,        KC_RGUI,        OSL(1),
                                                                     KC_SPC,         KC_TAB,         TG(1),                         TG(1),          KC_ENT,         KC_BSPC
