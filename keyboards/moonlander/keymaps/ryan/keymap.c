09e6945c5b075b2a80803efd3fa8c6c68a37c3b4
Ryan Gibb <ryan@freumh.org>
2023-09-12 20:21:05 +0100

    move raise modifier to corners

--- keyboards/moonlander/keymaps/ryan/keymap.c
+++ keyboards/moonlander/keymaps/ryan/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(3),                         KC_EQUAL,       KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_DEL,
     UK_GRV,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           TG(2),                         KC_MINUS,       KC_H,           KC_J,           KC_K,           KC_L,           KC_SEMICOLON,   UK_QUOT,
     UK_BSLS,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLSH,        UK_HASH,
-    XXXXXXX,        KC_LGUI,        KC_LALT,        KC_LCTL,        KC_LSFT,                        OSL(1),                        OSL(1),                         KC_RSFT,        KC_RCTL,        KC_RALT,        KC_RGUI,        XXXXXXX,
+    OSL(1),         KC_LGUI,        KC_LALT,        KC_LCTL,        KC_LSFT,                        XXXXXXX,                       XXXXXXX,                        KC_RSFT,        KC_RCTL,        KC_RALT,        KC_RGUI,        OSL(1),
                                                                     KC_SPC,         KC_TAB,         TG(1),                         TG(1),          KC_ENT,         KC_BSPC
   ),
   // raise
