079d830c4925828cd1dbb618441bef071185bbfb
Ryan Gibb <ryan@freumh.org>
2023-09-13 12:41:56 +0100

    custom leds

--- keyboards/moonlander/keymaps/ryan/config.h
+++ keyboards/moonlander/keymaps/ryan/config.h
@@ -3,6 +3,7 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
+#define MOONLANDER_USER_LEDS
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define CAPS_LOCK_STATUS
 #define LAYER_STATE_16BIT
