33500bc750e1e90eab97625b3080133cefd2367d
Ryan Gibb <ryan@freumh.org>
2024-03-05 13:40:05 +0000

    update

--- keyboards/moonlander/keymaps/ryan/keymap.c
+++ keyboards/moonlander/keymaps/ryan/keymap.c
@@ -16,8 +16,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // base
   [0] = LAYOUT_moonlander(
-    XXXXXXX,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TG(3),                         TG(5),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           XXXXXXX,
-    XXXXXXX,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                         XXXXXXX,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           XXXXXXX,
+    KC_VOLU,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TG(3),                         TG(5),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BRIU,
+    KC_VOLD,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                         XXXXXXX,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BRID,
     UK_GRV,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           TG(1),                         TG(4),          KC_H,           KC_J,           KC_K,           KC_L,           KC_SEMICOLON,   UK_QUOT,
     UK_BSLS,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLSH,        UK_HASH,
     KC_RALT,        KC_LGUI,        KC_LALT,        KC_LCTL,        KC_LSFT,                        KC_ESC,                        KC_DEL,                         KC_RSFT,        KC_RCTL,        KC_LALT,        KC_RGUI,        KC_RALT,
@@ -26,28 +26,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // raise
   [1] = LAYOUT_moonlander(
     XXXXXXX,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          XXXXXXX,                       XXXXXXX,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-    XXXXXXX,        KC_INS,         XXXXXXX,        KC_BRID,        KC_BRIU,        KC_PSCR,        XXXXXXX,                       XXXXXXX,        _______,        KC_HOME,        KC_END,         KC_LBRC,        KC_RBRC,        KC_F12,
-    XXXXXXX,        KC_SCRL,        KC_MPRV,        KC_VOLD,        KC_VOLU,        KC_MNXT,        _______,                       XXXXXXX,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_MINUS,       KC_EQUAL,
-    XXXXXXX,        KC_PAUS,        KC_MSTP,        KC_MPLY,        KC_MUTE,        XXXXXXX,                                                       _______,        KC_PGUP,        KC_PGDN,        KC_APP,         CW_TOGG,        KC_CAPS_LOCK,
-    XXXXXXX,        _______,        _______,        _______,        _______,                        XXXXXXX,                       XXXXXXX,                        _______,        _______,        _______,        _______,        XXXXXXX,
+    KC_INS,         XXXXXXX,        XXXXXXX,        S(KC_LBRC),     S(KC_RBRC),     XXXXXXX,        XXXXXXX,                       XXXXXXX,        XXXXXXX,        KC_PGDN,        KC_PGUP,        XXXXXXX,        KC_PSCR,        KC_F12,
+    KC_SCRL,        KC_MPRV,        KC_MNXT,        KC_LBRC,        KC_RBRC,        XXXXXXX,        _______,                       XXXXXXX,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       CW_TOGG,        XXXXXXX,
+    KC_PAUS,        KC_MPLY,        KC_MUTE,        KC_MINUS,       KC_EQUAL,       XXXXXXX,                                                       XXXXXXX,        KC_HOME,        KC_END,         KC_APP,         KC_CAPS_LOCK,   XXXXXXX,
+    _______,        _______,        _______,        _______,        _______,                        XXXXXXX,                       XXXXXXX,                        _______,        _______,        _______,        _______,        _______,
                                                                     XXXXXXX,        XXXXXXX,        XXXXXXX,                       XXXXXXX,        XXXXXXX,        XXXXXXX
   ),
   // numpad
   [2] = LAYOUT_moonlander(
-    XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,                       XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
-    XXXXXXX,        _______,        _______,        _______,        _______,        _______,        _______,                       XXXXXXX,        XXXXXXX,        KC_7,           KC_8,           KC_9,           S(KC_EQUAL),    XXXXXXX,
-    XXXXXXX,        _______,        _______,        _______,        _______,        _______,        XXXXXXX,                       XXXXXXX,        XXXXXXX,        KC_4,           KC_5,           KC_6,           _______,        _______,
-    XXXXXXX,        _______,        _______,        _______,        _______,        _______,                                                       XXXXXXX,        KC_1,           KC_2,           KC_3,           KC_SLSH,        XXXXXXX,
-    XXXXXXX,        _______,        _______,        _______,        _______,                        _______,                       _______,                        KC_0,           KC_COMMA,       KC_DOT,         S(KC_8),        XXXXXXX,
+    _______,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,                       XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
+    _______,        _______,        _______,        _______,        _______,        _______,        _______,                       XXXXXXX,        XXXXXXX,        KC_7,           KC_8,           KC_9,           S(KC_EQUAL),    XXXXXXX,
+    _______,        _______,        _______,        _______,        _______,        _______,        XXXXXXX,                       XXXXXXX,        XXXXXXX,        KC_4,           KC_5,           KC_6,           KC_MINUS,       _______,
+    _______,        _______,        _______,        _______,        _______,        _______,                                                       XXXXXXX,        KC_1,           KC_2,           KC_3,           KC_SLSH,        XXXXXXX,
+    _______,        _______,        _______,        _______,        _______,                        _______,                       _______,                        KC_0,           KC_COMMA,       KC_DOT,         S(KC_8),        XXXXXXX,
                                                                     _______,        _______,        _______,                       _______,        _______,        _______
   ),
   // mouse
   [3] = LAYOUT_moonlander(
-    XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        _______,                       XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
-    XXXXXXX,        _______,        _______,        _______,        _______,        _______,        XXXXXXX,                       XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
-    XXXXXXX,        _______,        _______,        _______,        _______,        _______,        XXXXXXX,                       XXXXXXX,        KC_MS_L,        KC_MS_D,        KC_MS_U,        KC_MS_R,        XXXXXXX,        XXXXXXX,
-    XXXXXXX,        _______,        _______,        _______,        _______,        _______,                                                       KC_WH_L,        KC_WH_D,        KC_WH_U,        KC_WH_R,        XXXXXXX,        XXXXXXX,
-    XXXXXXX,        _______,        _______,        _______,        _______,                        _______,                       _______,                        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
+    _______,        _______,        _______,        _______,        _______,        _______,        _______,                       XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
+    _______,        _______,        _______,        _______,        _______,        _______,        XXXXXXX,                       XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
+    _______,        _______,        _______,        _______,        _______,        _______,        XXXXXXX,                       XXXXXXX,        KC_MS_L,        KC_MS_D,        KC_MS_U,        KC_MS_R,        XXXXXXX,        XXXXXXX,
+    _______,        _______,        _______,        _______,        _______,        _______,                                                       KC_WH_L,        KC_WH_D,        KC_WH_U,        KC_WH_R,        XXXXXXX,        XXXXXXX,
+    _______,        _______,        _______,        _______,        _______,                        _______,                       _______,                        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
                                                                     KC_BTN1,        KC_BTN2,        KC_BTN3,                       KC_BTN3,        KC_BTN2,        KC_BTN1
   ),
   // game
