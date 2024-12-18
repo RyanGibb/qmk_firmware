d3948c13f7b4a0df6bb03264303ad05ed65af8a1
Ryan Gibb <ryan@freumh.org>
2024-12-18 15:01:20 +0000

    update

--- keyboards/moonlander/keymaps/ryan/keymap.c
+++ keyboards/moonlander/keymaps/ryan/keymap.c
@@ -16,8 +16,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // base
   [0] = LAYOUT_moonlander(
-    KC_MINUS,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TG(3),                         TG(5),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQUAL,
-    KC_LBRC,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                         XXXXXXX,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_RBRC,
+    KC_MINUS,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TG(3),                         XXXXXXX,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQUAL,
+    KC_LBRC,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                         TG(5),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_RBRC,
     UK_GRV,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           TG(1),                         TG(4),          KC_H,           KC_J,           KC_K,           KC_L,           KC_SEMICOLON,   UK_QUOT,
     UK_BSLS,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLSH,        UK_HASH,
     KC_RALT,        KC_LGUI,        KC_LALT,        KC_LCTL,        KC_LSFT,                        KC_ESC,                        KC_DEL,                         KC_RSFT,        KC_RCTL,        KC_LALT,        KC_RGUI,        KC_RALT,
